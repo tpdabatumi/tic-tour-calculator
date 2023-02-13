b35a9ae285841246d0090026b937ec3469e92689
Kajaia <lashakajaia31@gmail.com>
2023-02-13 23:57:47 +0200

    Migrated to vite

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-import store from "./store";
-import bootstrap from "bootstrap";
 
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.esm.js";
 
-createApp(App).use(store).use(router).use(bootstrap).mount("#app");
+const app = createApp(App);
+
+app.use(router);
+
+app.mount("#app");
