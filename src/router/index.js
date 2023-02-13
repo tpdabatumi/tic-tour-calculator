5a4a57905c2c8a68dfeed31843f13c9059265155
Kajaia <lashakajaia31@gmail.com>
2023-02-13 23:30:45 +0200

    Updated dependencies

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
 import Machakhela from "../views/Machakhela.vue";
 import Wine from "../views/Wine.vue";
@@ -70,7 +70,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes,
 });
 
